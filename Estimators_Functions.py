08c6f1b52b1304faef56ce13371b806db8f43044
viscontitommi <167067123+viscontitommi@users.noreply.github.com>
2025-12-01 20:20:29 +0100

    Add files via upload

---
+++ Estimators_Functions.py
@@ -0,0 +1,183 @@
+import numpy as np
+import pandas as pd
+import math as m
+import scipy.stats as stats
+import Functions_GBM as GBM
+import Functions_Variance as f_var
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+def add_micro_noise(price_series, n_paths):
+    noise = pd.DataFrame(np.random.normal(0, 0.005, (len(price_series), n_paths)))
+    df_obs_price = price_series + noise
+    return df_obs_price
+
+def tsrv_estimator(prices, K): #K=300 is 5 minutes
+    n = len(prices)
+    log_prices = np.log(prices)
+    
+    # this estimate contains both real volatility and 2*n*E[noise^2]
+    rv_all = f_var.calculate_mean_rv2(f_var.calculate_rv_per_path2(prices)) #Fast Scale 
+    
+    rv_subgrids = [] # K Subgrids for Slow Scale
+    for k in range(K):
+        subgrid_prices = prices[k::K] 
+        rv_sub = f_var.calculate_mean_rv2(f_var.calculate_rv_per_path2(subgrid_prices))
+        rv_subgrids.append(rv_sub)
+
+    rv_avg = np.mean(rv_subgrids) # Subgrids Mean
+
+    n_bar = (n - K + 1) / K # constant for correction
+    constraint_factor = 1 / (1 - (n_bar / n)) # Correction for small samples, at the beginning of formula
+    
+    tsrv = constraint_factor * (rv_avg - (n_bar / n) * rv_all) # formula for Zhang Estimator
+    
+    return np.array(tsrv)
+
+def calculate_error(est1, benchmark):
+    err = abs(est1 - benchmark) / benchmark
+    return np.array(err)
+
+def generate_tsrv_table(prices, true_iv, k_range, k_chosen):
+    tsrv_chosen = tsrv_estimator(prices, k_chosen) # K chosen from user
+    
+    k_values = list(k_range)
+    estimates_list = []
+    errors_list = []
+    
+    for k in k_values:
+        est = tsrv_estimator(prices, k)
+        estimates_list.append(est)
+    
+        err = (est - true_iv)**2 #MSE
+        errors_list.append(err)
+    tsrv_grid = np.vstack(estimates_list) # Matrice (n_k_values x n_paths)
+    errors_grid = np.vstack(errors_list)
+    
+    best_indices = np.argmin(errors_grid, axis=0) #best K through the columns, K value for every path
+    n_paths = tsrv_grid.shape[1] # extract optimal values
+    
+    optimal_k = []
+    tsrv_optimal_values = []
+    for i in range(n_paths):
+        idx_best = best_indices[i] # Indice del K migliore per il path 'i'
+        optimal_k.append(k_values[idx_best]) # Valore di K corrispondente
+        tsrv_optimal_values.append(tsrv_grid[idx_best, i]) # Valore di TSRV corrispondente (riga=idx_best, colonna=i)
+        
+    if isinstance(prices, pd.DataFrame): # 5. Costruisci il DataFrame finale
+        path_names = prices.columns
+    else:
+        path_names = [f"Path_{i}" for i in range(n_paths)]
+        
+    results_df = pd.DataFrame({
+        'Optimal K': optimal_k,
+        'TSRV (Optimal K)': tsrv_optimal_values,
+        f'TSRV (K={k_chosen})': tsrv_chosen
+    }, index=path_names)
+    
+    return results_df
+
+def plot_consistency_check(mu, sigma, S0, noise_std, n_paths=1000):
+    n_steps_list = [25, 50, 75, 100, 150, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 3500, 5000] # testing frequencies
+    target_iv = sigma**2 * (1/252) # Target giornaliero (approx)
+    
+    rv_means = []
+    tsrv_means = []
+    for n in n_steps_list:
+        dt = 1 / (252 * n)
+        
+        prices = GBM.sim_gbm_paths(n_paths, n, mu, sigma, S0, dt, seed=42)
+        noise = np.random.normal(0, noise_std, prices.shape)
+        noisy_prices = prices + noise
+        
+        rv_vec = f_var.calculate_rv_per_path2(noisy_prices)
+        rv_means.append(np.mean(rv_vec))
+        
+        # 4. Calcola TSRV
+        K = int(0.5 * (n**(2/3))) # Regola euristica per K
+        if K < 1: K = 1
+        
+        # Chiamata alla funzione TSRV che hai già in questo file
+        tsrv_vec = tsrv_estimator(noisy_prices, K)
+        tsrv_means.append(np.mean(tsrv_vec))
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(n_steps_list, rv_means, 'r-o', label='Realized Volatility (RV)', linewidth=1)
+    plt.plot(n_steps_list, tsrv_means, 'g-s', label='Two-Scales RV (TSRV)', linewidth=1)
+    plt.axhline(y=target_iv, color='blue', linestyle='--', label='True IV')
+    plt.title(f"Volatility Signature Plot\n(Noise={noise_std})")
+    plt.xlabel("Sampling Frequency (N steps/day)")
+    plt.ylabel("Estimated Volatility")
+    plt.legend()
+    plt.grid(True, alpha=0.5)
+    plt.show()
+
+def run_signature_plot(mu, sigma, S0, noise_std, n_paths):
+    freq_list = [500, 1000, 5000, 10000, 23400] # Da bassa ad alta frequenza
+    target_iv = (sigma**2) * (1/252) # Target giornaliero (approx)
+    
+    rv_means = []
+    tsrv_means = []
+    
+    for n in freq_list:
+        dt = 1 / (252 * n)
+    
+        prices = GBM.sim_gbm_paths(n_paths, n, mu, sigma, S0, dt)
+        
+        noise = pd.DataFrame(np.random.normal(0, noise_std, prices.shape))
+        noisy = prices + noise
+        
+        rv_vec = f_var.calculate_rv_per_path2(noisy)
+        rv_means.append(np.mean(rv_vec))
+        
+        K = int(0.5 * (n**(2/3)))
+        if K < 1: K = 1
+        tsrv_vec = tsrv_estimator(noisy, K) # Usa la tua funzione tsrv_estimator
+        tsrv_means.append(np.mean(tsrv_vec))
+        
+    plt.figure(figsize=(10, 6))
+    plt.plot(freq_list, rv_means, 'r-o', label='Realized Volatility (RV)')
+    plt.plot(freq_list, tsrv_means, 'g-s', label='TSRV')
+    plt.axhline(y=target_iv, color='blue', linestyle='--', label='True IV')
+    plt.title("Volatility Signature Plot")
+    plt.xlabel("Frequency (N steps)")
+    plt.ylabel("Volatility")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.show()
+
+
+def calculate_expected_noise_error(n, noise_std):
+    """
+    Calculates the expected bias in Realized Volatility due to microstructure noise.
+    """
+    noise_variance = np.array(noise_std ** 2)
+    expected_bias = np.array(2 * n * noise_variance)
+    return expected_bias
+
+
+def calculate_expected_discretization_error(n, noise_std):
+    """
+    Calculates the expected magnitude (standard deviation) of the stochastic error 
+    term O_p(n^(1/2)) for Gaussian noise.
+    """
+    noise_variance = np.array(noise_std ** 2)
+    fourth_moment = np.array(3 * (noise_variance ** 2)) # For Gaussian noise: E[epsilon^4] = 3 * (variance^2) = 3 * noise_std^4
+    estimator_variance = np.array(4 * n * fourth_moment) # Variance of the RV estimator due to noise is approx 4 * n * E[epsilon^4]
+    stochastic_error_magnitude = np.sqrt(estimator_variance) #std. dev
+    return stochastic_error_magnitude
+
+def plot_theoretical_errors(noise_std, max_n=20000):
+    n_values = np.arange(100, max_n, 100)  # Generiamo un range di frequenze n
+    noise_bias = calculate_expected_noise_error(n_values, noise_std)
+    discretization_error = calculate_expected_discretization_error(n_values, noise_std)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(n_values, noise_bias, 'r-', linewidth=2, label=r'Noise Bias ($2n\mathbb{E}[\epsilon^2]$)')
+    plt.plot(n_values, discretization_error, 'b--', linewidth=2, label=r'Discretization Error ($O_p(\sqrt{n})$)')
+    plt.title(f"Theoretical COmparison of Errors (Noise Std = {noise_std})")
+    plt.xlabel("Sampling frequency ($n$)")
+    plt.ylabel("Magnitude of Error (Variance)")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.show()
---
+++ Functions_GBM.py
@@ -0,0 +1,131 @@
+import math as m
+from turtle import rt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from IPython.display import display
+
+#Simulation of multiple GBM paths
+def compute_gbm_log_ret(T, n_paths, mu, sigma, dt):
+    err = np.random.normal(0, 1, (T, n_paths))
+    drift_term = (mu - 0.5 * sigma**2) * dt
+    diff_term = sigma * np.sqrt(dt) * err
+    log_ret = drift_term + diff_term
+    return pd.DataFrame(log_ret)
+
+def compute_prices(log_ret, S0, n_paths):
+    ini_log_pr = np.log(S0)
+    cumulative_log_returns = np.cumsum(log_ret, axis=0) # Cumulative sum along each path
+    start_row = np.full((1, n_paths), ini_log_pr) # Initial row with S0 log-price for each path
+    log_prices = np.vstack([start_row, ini_log_pr + cumulative_log_returns]) # Add first row to cumulative sums
+    prices = np.exp(log_prices)
+    return pd.DataFrame(prices)
+
+def sim_gbm_paths(n_paths, T, mu, sigma, S0, dt, seed=42):
+    np.random.seed(seed)
+    log_ret = compute_gbm_log_ret(T, n_paths, mu, sigma, dt)
+    prices = compute_prices(log_ret, S0, n_paths)
+    return prices
+
+def plot_gbm_paths(ts_prices):
+    plt.figure(figsize=(12, 6))
+    plt.plot(ts_prices, alpha=0.6, linewidth=1)
+    n_paths = ts_prices.shape[1]
+    plt.title(f"Stock Price Simulation (GBM) - {n_paths} Scenarios")
+    plt.xlabel("Days")
+    plt.ylabel("Price ($)")
+    plt.show()
+
+# Statistics GBM
+def statistics_gbm(prices):
+    print("=" * 60)
+    print("       EMPIRICAL GBM STATISTICS (Log-Returns)")
+    print("=" * 60)
+    log_rets = np.log(prices / prices.shift(1)).dropna()
+    means = log_rets.mean()
+    vars = log_rets.var()
+    skews = log_rets.skew()
+    kurts = log_rets.kurt() # Excess kurtosis (Normal = 0)
+    stats_df = pd.DataFrame({
+        'Mean': means,
+        'Variance': vars,
+        'Skewness': skews,
+        'Kurtosis': kurts
+    })
+    print(f"Paths Drifts Mean:      {stats_df['Mean'].mean():.6f}")
+    print(f"Paths Variance Mean:  {stats_df['Variance'].mean():.6f}")
+    print(f"Paths Skewness Mean:   {stats_df['Skewness'].mean():.4f}")
+    print(f"Paths Kurtosis Mean:   {stats_df['Kurtosis'].mean():.4f}")
+    
+    print("=" * 60)
+    # Jarque-Bera Test
+    if stats.jarque_bera(log_rets)[1] > 0.05:
+        print("Normal Returns Assumption Validated through Jarque-Bera Test.")
+    else:
+        print("Not validated through Jarque-Bera Test.")
+
+
+    return stats_df
+
+# MARTINGALITY
+def martingale(prices, dt, mu):
+    """
+    Prepara i dati per il test di martingala scontando i prezzi.
+    
+    Parametri:
+    prices (pd.DataFrame): DataFrame dei prezzi simulati (Tempo x Path).
+    dt (float): Passo temporale (es. 1/252).
+    mu (float): Drift utilizzato nella simulazione (tasso di sconto).
+    
+    Return:
+    martingale_paths (pd.DataFrame): Tutti i percorsi scontati.
+    mean_path (pd.Series): La media empirica dei percorsi scontati.
+    """
+    n_steps = len(prices)
+    time_grid = np.arange(n_steps) * dt
+    discount_factors = np.exp(-mu * time_grid)
+    martingale_paths = prices.multiply(discount_factors, axis=0)
+    mean_path = martingale_paths.mean(axis=1)
+
+    return martingale_paths, mean_path
+
+# Visual Test of Martingality
+def plot_martingale_test(martingale_paths, mean_path, S0):
+    plt.figure(figsize=(12, 6))
+    n_plot = min(100, martingale_paths.shape[1])
+    plt.plot(martingale_paths.iloc[:, :n_plot], color='grey', alpha=0.1, linewidth=0.5)
+    
+    plt.plot(mean_path, color='red', linewidth=2.5, label='Empirical Mean (Monte Carlo)')
+    
+    plt.axhline(y=S0, color='blue', linestyle='--', linewidth=2, label=f'Theoretical Value ({S0})')
+    
+    plt.title("Visual Test of Martingality (Discounted Prices)")
+    plt.xlabel("Steps")
+    plt.ylabel("Discounted Value ($)")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    
+    plt.show()
+
+# Statistical Tests of Martingality
+def test_martingale_statistics(martingale_paths, S0, alpha=0.05):
+    print("=" * 60)
+    print("       MARTINGALITY T-TESTS ON DISCOUNTED PRICES       ")
+    print("=" * 60)
+    
+    final_values = martingale_paths.iloc[-1, :] #discounted price at the end of the simulation, T
+    
+    t_stat_final, p_val_final = stats.ttest_1samp(final_values, popmean=S0) # H0: Final Value Mean = S0
+    
+    print(f"Final Empirical Mean: {final_values.mean():.4f} (Target: {S0})")
+    print(f"T-Statistic: {t_stat_final:.4f}")
+    print(f"P-Value:     {p_val_final:.4f}")
+    
+    if p_val_final > alpha:
+        print(">> Do not Reject H0. IT IS A MARTINGALE.")
+    else:
+        print(">> Reject H0. NOT A MARTINGALE.")
+
+
+
